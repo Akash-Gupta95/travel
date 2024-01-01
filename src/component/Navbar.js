bfdb7b26e37ec7e495ab497634be5da43082b9d6
akash gupta <akash@AkashGupta>
2024-01-01 21:04:28 +0530

    nav

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -25,7 +25,7 @@ const Navbar = () => {
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
           <a className="nav-link active" aria-current="page" href="#home">
-            Home
+            Happy Home
           </a>
         </li>
         <li className="nav-item">
